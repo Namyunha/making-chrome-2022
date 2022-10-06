182e5bfe9045c45cf05183d7d6e0214a192d917e
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-10-06 16:17:46 +0900

    ClockAndGreetings

---
+++ js/qutes.js
@@ -0,0 +1,49 @@
+const quotes = [
+  {
+    quote: "웃을 수 없다면 우리는 모두 돌아버릴 것이다.",
+    author: "Jimmy Buffett",
+  },
+  {
+    quote:
+      "희망은 날개 달린 것, 영혼에 내려앉아 가사 없는 노래 부르네. 그치지 않는 그 노래.",
+    author: "Emily Dickinson",
+  },
+  {
+    quote:
+      "함께 있을 때 웃음이 나오지 않는 사람과는 결코 진정한 사랑에 빠질 수 없다.",
+    author: "Agnes Repplier",
+  },
+  {
+    quote:
+      "인간의 심장에서 희망을 빼앗아라. 그럼 그는 먹이를 찾는 야수가 될 것이다.",
+    author: "Quida",
+  },
+  {
+    quote:
+      "미래를 창조하기에 꿈만큼 좋은 것은 없다. 오늘의 유토피아가 내일 현실이 될 수 있다.",
+    author: "Victor Hugo",
+  },
+  {
+    quote:
+      "어릴 적 나에겐 정말 많은 꿈이 있었고, 그 꿈의 대부분은 많은 책을 읽을 기회가 많았기에 가능했다고 생각한다.",
+    author: "Bill Gates",
+  },
+  {
+    quote: "세상 모든 일은 여러분이 무엇을 생각하느냐에 따라 일어납니다.",
+    author: "Oprah Winfrey",
+  },
+  {
+    quote: "유머의 부재는 인생을 불가능으로 바꾼다.",
+    author: "Colette",
+  },
+  {
+    quote:
+      "얼굴이 계속 햇빛을 향하도록 하라. 그러면 당신의 그림자를 볼 수 없다.",
+    author: "Helen Keller",
+  },
+  {
+    quote:
+      "나는 운명처럼 웃음과 약혼했다. 웃음소리는 언제나 세상에서 가장 세련된 음악으로 들린다.",
+    author: "Havelock Ellis",
+  },
+];
